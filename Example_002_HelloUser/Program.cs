cc884ab8ff35345a148017941f42e92765e115f3
My Name <Me@gmail.com>
2022-08-04 19:19:49 +0400

    created gitignore

--- Example_002_HelloUser/Program.cs
+++ Example_002_HelloUser/Program.cs
@@ -1,5 +1,7 @@
-﻿Console.WriteLine("Введите ваше имя: ");
+﻿Console.Write("Введите ваше имя: ");
 string username = Console.ReadLine();
-Console.WriteLine("Привет, ");
-Console.WriteLine(username);
+Console.Write("Привет, ");
+Console.Write(username);
+// string username - в C# перед переменной обязательно нужно указывать тип данных, который она будет хранить
+
 
